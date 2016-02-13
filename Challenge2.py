c566b7420efc36905609ad9cc3c2062a536995a3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 13:55:55 -0500

    Updated from Brython Server: 2/13/2016 1:55:55 PM

--- Challenge2.py
+++ Challenge2.py
@@ -26,36 +26,41 @@ for x in abc:
     print(x[0]+x[1]+x[2])
 '''
 
-money = 89
+money = 1
 
-a = list(range(0,money//7+1))
-b = list(range(0,money//15+1))
-c = list(range(0,money//23+1))
+while money != 0:
+    money = int(input("How much money do you have (stop with 0)? "))
 
-anew = len(c)*((a*len(b)).sort())
-bnew = len(c)*(b*len(a))
-cnew = (c*len(a)*len(b)).sort()
+    a = list(range(0,money//7+1))
+    b = list(range(0,money//15+1))
+    c = list(range(0,money//23+1))
 
-abc = zip(anew, bnew, cnew)
+    anew = len(c)*((a*len(b)).sort())
+    bnew = len(c)*(b*len(a))
+    cnew = (c*len(a)*len(b)).sort()
 
+    abc = zip(anew, bnew, cnew)
+
+    success = "false"
+
+    for x in abc:
+        cost = 7*x[0]+15*x[1]+23*x[2]
+        if cost == money:
+         if success == "false":
+                message = "You can spend all your money: "
+            else:
+                message = "                              "
+            print(message+"Hat: "+str(x[0])+" Shirt: "+str(x[1])+" Jacket: "+str(x[2]))
+            success = "true"
+        
+    if success == "false":
+        print("You cannot spend all your money!")
+
+    success = "false"
+
+'''
 print(anew)
 print(bnew)
 print(cnew)
 print(list(abc))
-
-success = "false"
-
-for x in abc:
-    cost = 7*x[0]+15*x[1]+23*x[2]
-    if cost == money:
-        if success == "false":
-            message = "You can spend all your money:  "
-        else:
-            message = "                               "
-        print(message+"Hat: "+str(x[0])+" Shirt: "+str(x[1])+" Jacket: "+str(x[2]))
-        success = "true"
-        
-if success == "false":
-    print("You cannot spend all your money!")
-
-success = "false"
+'''
