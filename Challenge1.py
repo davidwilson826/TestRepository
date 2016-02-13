b4aca2273c9a45e5fc87bca491217672ee4359af
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-12 23:19:05 -0500

    Updated from Brython Server: 2/12/2016 11:19:04 PM

--- Challenge1.py
+++ Challenge1.py
@@ -1,3 +1,10 @@
-cubes = [1,8]
-sums = [x*cubes for x in cubes]
+cubes = [1]
+sums = [1,1]
+cubes = cubes+8
+sums = sums+[x+8 for x in cubes]
+
+for x in sums.sort():
+    if sums.count(x) > 1:
+        print(x)
+
 print(sums)
