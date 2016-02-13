c7c14f6ab6072d46f75ad044f17529ed8dee79c8
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-12 23:20:04 -0500

    Updated from Brython Server: 2/12/2016 11:20:03 PM

--- Challenge1.py
+++ Challenge1.py
@@ -1,10 +1,12 @@
+done = "false"
 cubes = [1]
 sums = [1,1]
 cubes = cubes+8
 sums = sums+[x+8 for x in cubes]
 
 for x in sums.sort():
-    if sums.count(x) > 1:
+    if sums.count(x) > 1 and done == "false":
         print(x)
+        done = "true"
 
 print(sums)
