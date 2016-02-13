28924000bd64bfb8b4b2bb5cc9310d337eb03ba1
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 14:33:59 -0500

    Updated from Brython Server: 2/13/2016 2:33:56 PM

--- Challenge3.py
+++ Challenge3.py
@@ -1,1 +1,14 @@
+string = input("Enter string (q/Q for stop)?	")
 
+if string[0] == 1:
+    output = "0"
+    
+nextnum = 0
+
+while len(string) > 0:
+    if string[0] == 0 and string[1] == 0:
+        nextnum += 1
+    elif string [0] == 0:
+        nextnum += 1
+        output += " "+str(nextnum)
+        nextnum = 0
