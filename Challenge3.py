f4e8804751adb60740a11070b2a3be78af8bf885
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 16:06:38 -0500

    Updated from Brython Server: 2/13/2016 4:06:37 PM

--- Challenge3.py
+++ Challenge3.py
@@ -45,16 +45,21 @@ string = input("Enter string (q/Q for stop)?	")
 
 nextnum = 0
 
-if string[0] == 0:
-    output = "a"
-else:
+if string[0] == "1":
     output = "a 0"
+else:
+    output = "a"
 
 while len(string) > 1:
     nextnum += 1
-    if string[0] != string[1] or len(string) == 2:
+    if string[0] != string[1]:
         output += " "+str(nextnum)
         nextnum = 0
+    if len(string) == 2:
+        if string[0] == string[1]:
+            output = output[:-1]+str(int(output[-1]+1))
+        else:
+            output = output+" 1"
     string = string[1:]
     
-print(output[2:-1]+str(int(output[-1])+1))
+print(output[2:])
