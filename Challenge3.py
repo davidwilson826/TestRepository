1f8c476e915bca9540f731d1102e5e31323b3810
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 15:13:50 -0500

    Updated from Brython Server: 2/13/2016 3:13:50 PM

--- Challenge3.py
+++ Challenge3.py
@@ -7,14 +7,14 @@ while len(string) > 1:
 
 print("done")
 
-'''
+
 string = input("Enter string (q/Q for stop)?	")
-'''
+
 if string[0] == 1:
     output = "a0"
 else:
     output = "a"
-'''
+
 nextnum = 0
 
 while len(string) > 1:
@@ -24,6 +24,14 @@ while len(string) > 1:
             output += " "+str(nextnum)
             nextnum = 0
         string = string[1:]
-'''
+
 print(output[1:])
 '''
+
+string = input("Enter string (q/Q for stop)?	")
+
+while len(string) > 1:
+    string = string[1:]
+    print(string)
+    
+print("done")
