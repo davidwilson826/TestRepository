8bb8b87546cb2cc10efe637b1b5b61b9ec85b309
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 12:40:00 -0500

    Updated from Brython Server: 2/13/2016 12:39:56 PM

--- Challenge2.py
+++ Challenge2.py
@@ -1,1 +1,14 @@
+'''
 money = int(input("How much money do you have (stop with 0)? "))
+
+hat = 7
+shirt = 15
+jacket = 23
+'''
+
+a = [1,2,3]
+b = [1,2,3]
+c = [1,2,3]
+
+a = (a*len(b)).sort()
+print(a)
