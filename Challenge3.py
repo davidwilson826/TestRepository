4e675dffb712c46c188a76445533ba8a282fcdb0
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 14:37:35 -0500

    Updated from Brython Server: 2/13/2016 2:37:34 PM

--- Challenge3.py
+++ Challenge3.py
@@ -6,9 +6,9 @@ if string[0] == 1:
 nextnum = 0
 
 while len(string) > 0:
-    if string[0] == 0 and string[1] == 0:
+    if string[0] == 0:
         nextnum += 1
-    elif string [0] == 0:
-        nextnum += 1
-        output += " "+str(nextnum)
-        nextnum = 0
+        if string[1] == 0:
+            output += " "+str(nextnum)
+            nextnum = 0
+        string = string[1:]
