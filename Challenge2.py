700921c3a03610d968da42b0a9e72fa4dbb53584
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-13 14:04:47 -0500

    Updated from Brython Server: 2/13/2016 2:04:46 PM

--- Challenge2.py
+++ Challenge2.py
@@ -30,33 +30,31 @@ money = 1
 
 while money != 0:
     money = int(input("How much money do you have (stop with 0)? "))
+    if money != 0:
+        a = list(range(0,money//7+1))
+        b = list(range(0,money//15+1))
+        c = list(range(0,money//23+1))
 
-    a = list(range(0,money//7+1))
-    b = list(range(0,money//15+1))
-    c = list(range(0,money//23+1))
+        anew = len(c)*((a*len(b)).sort())
+        bnew = len(c)*(b*len(a))
+        cnew = (c*len(a)*len(b)).sort()
 
-    anew = len(c)*((a*len(b)).sort())
-    bnew = len(c)*(b*len(a))
-    cnew = (c*len(a)*len(b)).sort()
+        abc = zip(anew, bnew, cnew)
 
-    abc = zip(anew, bnew, cnew)
+        success = "false"
 
-    success = "false"
-
-    for x in abc:
-        cost = 7*x[0]+15*x[1]+23*x[2]
-        if cost == money:
-            if success == "false":
-                message = "You can spend all your money: "
-            else:
-                message = "                              "
-            print(message+"Hat: "+str(x[0])+" Shirt: "+str(x[1])+" Jacket: "+str(x[2]))
-            success = "true"
+        for x in abc:
+            cost = 7*x[0]+15*x[1]+23*x[2]
+            if cost == money:
+                if success == "false":
+                    message = "You can spend all your money: "
+                else:
+                    message = "                              "
+                print(message+"Hat: "+str(x[0])+" Shirt: "+str(x[1])+" Jacket: "+str(x[2]))
+                success = "true"
         
-    if success == "false":
-        print("You cannot spend all your money!")
-
-    success = "false"
+        if success == "false":
+            print("You cannot spend all your money!")
 
 '''
 print(anew)
