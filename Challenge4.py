f74eab46e57fa2832307b56d9a52301924436a8d
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-14 12:28:30 -0500

    Updated from Brython Server: 2/14/2016 12:28:27 PM

--- Challenge4.py
+++ Challenge4.py
@@ -1,2 +1,8 @@
 string1 = input("Enter first string? ")
 string2 = input("Enter second string? ")
+
+for x in string1:
+    for y in string2:
+        if x == y:
+            cstring1 = string1[x-1:]
+            cstring2 = string2[y-1:]
