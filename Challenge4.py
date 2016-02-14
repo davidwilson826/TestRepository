29b2c8a25a4fd9d18b731f964b61fe43aae857e3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-14 16:15:09 -0500

    Updated from Brython Server: 2/14/2016 4:15:07 PM

--- Challenge4.py
+++ Challenge4.py
@@ -1,6 +1,6 @@
 
-string1 = "david" #input("Enter first string? ")
-string2 = "dog" #input("Enter second string? ")
+string1 = "aaaaabbccccc" #input("Enter first string? ")
+string2 = "aabbaaaaabcccc" #input("Enter second string? ")
 
 s1sl = list(string1)
 s2sl = list(string2)
@@ -12,23 +12,27 @@ s1l = list(zip(s1nl, s1sl))
 s2l = list(zip(s2nl, s2sl))
 
 cstring = "a"
+currcstring = "a"
 
 for x in s1l:
     for y in s2l:
         if x[1] == y[1]:
             clist1 = s1l[s1l.index(x):]
             clist2 = s2l[s2l.index(y):]
+
             charnum = 0
             while clist1[charnum][1] == clist2[charnum][1]:
-                cstring += clist1[charnum][1]
+                currcstring += clist1[charnum][1]
                 charnum += 1
-                clist1 = clist1[1:]
-                clist2 = clist2[1:]
+            if len(currcstring) > len(cstring):
+                cstring = currcstring
             print(clist1)
             print(clist2)
-            print(cstring)
-'''
+            print(cstring[1:])
+            print(charnum)
 
+
+'''
 a = [1,2,3]
 b = [1,2,3]
 
