ca775b57772aabbb245cb259ba57f1654315de0b
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-14 12:33:58 -0500

    Updated from Brython Server: 2/14/2016 12:33:56 PM

--- Challenge4.py
+++ Challenge4.py
@@ -4,5 +4,7 @@ string2 = input("Enter second string? ")
 for x in string1:
     for y in string2:
         if x == y:
-            cstring1 = string1[x-1:]
-            cstring2 = string2[y-1:]
+            cstring1 = string1[string1.find(x):]
+            cstring2 = string2[string2.find(y):]
+            print(cstring1)
+            print(cstring2)
