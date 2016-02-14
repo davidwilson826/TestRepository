08850aa05e466442d61e300a7fce245a59fb0c9b
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-14 13:34:20 -0500

    Updated from Brython Server: 2/14/2016 1:34:18 PM

--- Challenge4.py
+++ Challenge4.py
@@ -1,13 +1,19 @@
-string1 = input("Enter first string? ")
-string2 = input("Enter second string? ")
+string1 = "david" #input("Enter first string? ")
+string2 = "dog" #input("Enter second string? ")
 
-s1l = list(range(0, len(string1)))
-s2l = list(range(0, len(string2)))
+s1sl = list(string1)
+s2sl = list(string2)
 
-for x in string1:
-    for y in string2:
-        if x == y:
-            cstring1 = string1[string1.find(x):]
-            cstring2 = string2[string2.find(y):]
+s1nl = list(range(0, len(string1)))
+s2nl = list(range(0, len(string2)))
+
+s1l = zip(s1sl, s1nl)
+s2l = zip(s2sl, s2nl)
+
+for x in s1l:
+    for y in s2l:
+        if x[1] == y[1]:
+            cstring1 = s1l[s1l.find(x):]
+            cstring2 = s2l[s2l.find(y):]
             print(cstring1)
             print(cstring2)
