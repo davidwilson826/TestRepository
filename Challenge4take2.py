9e9834c6a4b3f8905293db200ece003a0e19b868
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-15 17:53:24 -0500

    Updated from Brython Server: 2/15/2016 5:53:23 PM

--- Challenge4take2.py
+++ Challenge4take2.py
@@ -2,15 +2,15 @@
 string1 = "doom"#input("Enter first string? ")
 string2 = "dog"#input("Enter second string? ")
 
-l1 = list(zip(list(string1), range(0, len(string1))))
-l2 = list(zip(list(string2), range(0, len(string2))))
+l1 = list(zip(range(0, len(string1)), list(string1)))
+l2 = list(zip(range(0, len(string2)), list(string2)))
 
 print(l1)
 print(l2)
 
 for x in l1:
     for y in l2:
-        if x == y:
+        if x[1] == y[1]:
             l1index = l1.index(x)
             l2index = l2.index(y)
             print(l1index)
