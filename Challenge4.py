bf63a9a5849dc88c259ef6fdbcdcce5178158584
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-15 17:34:51 -0500

    Updated from Brython Server: 2/15/2016 5:34:50 PM

--- Challenge4.py
+++ Challenge4.py
@@ -24,6 +24,12 @@ for x in s1l:
             while clist1[charnum][1] == clist2[charnum][1] and len(clist1)-1 > charnum and len(clist2)-1 > charnum:
                 currcstring += clist1[charnum][1]
                 charnum += 1
+            if len(clist1)-1 <= charnum:
+                if clist1[-1] == clist2[charnum]:
+                    currcstring += clist2[charnum][1]
+            elif len(clist2)-1 <= charnum:
+                if clist2[-1] == clist1[charnum]:
+                    currcstring += clist1[charnum][1]
             if len(currcstring) > len(cstring[-1]):
                 cstring = ["a", currcstring[1:]]
             elif len(currcstring)-1 == len(cstring[-1]):
