177f9ac50b2f0a8853fd4b94e6258a93ea038e93
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-15 17:51:59 -0500

    Updated from Brython Server: 2/15/2016 5:51:58 PM

--- Challenge4take2.py
+++ Challenge4take2.py
@@ -1,8 +1,9 @@
+
 string1 = "doom"#input("Enter first string? ")
 string2 = "dog"#input("Enter second string? ")
 
-l1 = list(string1)
-l2 = list(string2)
+l1 = list(zip(list(string1), range(0, len(string1))))
+l2 = list(zip(list(string2), range(0, len(string2))))
 
 print(l1)
 print(l2)
