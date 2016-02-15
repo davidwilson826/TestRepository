1255fae198831bb7c75c4989d4c990c9f3d2fef3
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-15 17:48:05 -0500

    Updated from Brython Server: 2/15/2016 5:48:04 PM

--- Challenge4take2.py
+++ Challenge4take2.py
@@ -1,1 +1,16 @@
+string1 = "doom"#input("Enter first string? ")
+string2 = "dog"#input("Enter second string? ")
 
+l1 = list(string1)
+l2 = list(string2)
+
+print(l1)
+print(l2)
+
+for x in l1:
+    for y in l2:
+        if x == y:
+            l1index = l1.index(x)
+            l2index = l2.index(y)
+            print(l1index)
+            print(l2index)
