45df33c78371420a19d4724a66c0dbf45989d4d0
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-15 18:01:16 -0500

    Updated from Brython Server: 2/15/2016 6:01:15 PM

--- Challenge4take2.py
+++ Challenge4take2.py
@@ -8,10 +8,26 @@ l2 = list(zip(range(0, len(string2)), list(string2)))
 print(l1)
 print(l2)
 
+cstring = ["a"]
+
 for x in l1:
     for y in l2:
         if x[1] == y[1]:
+            currstring = "a"
             l1index = l1.index(x)
             l2index = l2.index(y)
+            
+            while l1[l1index][1] == l2[l2index][1] and l1index <= len(l1)-1 and l2index <= len(l2)-1:
+                currstring += l1[l1index][1]
+                l1index += 1
+                l2index += 1
+            
+            if len(cstring[-1]) == len(currstring):
+                cstring += currstring
+            elif len(currstring) > len(cstring[-1]):
+                cstring = ["a", currstring]
+            
             print(l1index)
             print(l2index)
+            
+print(cstring)
