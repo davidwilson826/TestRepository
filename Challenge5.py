9b730db4523ffe0fbc2664cca97c71865b4b02a8
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-16 15:12:15 -0500

    Updated from Brython Server: 2/16/2016 3:12:13 PM

--- Challenge5.py
+++ Challenge5.py
@@ -5,15 +5,18 @@ size = 3 #input("Matrix size? ")
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 mlist = [alpha[randint(0, 25)] for x in [1]*size**2]
+print(mlist)
 
 print("Matrix Generated: ")
 
 pos = [0, 0]
 
 while (pos[0]+1)*(pos[1]+1) <= size**2:
-    print(" "+mlist[pos[0]*size+pos[1])
+    toprint = mlist[pos[0]*size+pos[1]]
     if pos[1]+1 == size:
+        print(toprint)
         pos[0] += 1
         pos[1] = 0
     else:
+        print(toprint, end=" ")
         pos[1] += 1
