52bb4f263e17e2cfe90f5c48bc84bcb5f9456d22
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-16 15:46:20 -0500

    Updated from Brython Server: 2/16/2016 3:46:18 PM

--- Challenge5.py
+++ Challenge5.py
@@ -1,3 +1,4 @@
+'''
 from random import randint
 
 size = 5 #input("Matrix size? ")
@@ -25,9 +26,20 @@ pos = [0, 0]
 nprint = 0
 hdist = size
 vdist = size
-direc = [1, 0]
+direc = [0, 1]
 
 while nprint < size**2-1:
     print(mlist[pos[0]*size+pos[1]], end=" ")
-    pos = [pos[0]+direc[1], pos[1]+direc[0]]
+    pos = [pos[0]+direc[0], pos[1]+direc[1]]
     nprint += 1
+'''
+
+def changedirec(x):
+    if x[1] == hdist:
+        x = [-1, 0]
+    elif x[1] == 0:
+        x = [1, 0]
+    elif x[0] == vdist:
+        x = [0, -1]
+    elif x[0] == 0:
+        x = [0, 1]
