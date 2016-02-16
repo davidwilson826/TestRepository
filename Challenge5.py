52705e80b36c3c5c1f170194db763e1d0985d13d
davidwilson826 <davidwilson@hanovernorwichschools.org>
2016-02-16 15:00:40 -0500

    Updated from Brython Server: 2/16/2016 3:00:38 PM

--- Challenge5.py
+++ Challenge5.py
@@ -1,1 +1,13 @@
+from random import randint
 
+size = 3 #input("Matrix size? ")
+
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+mlist = [alpha[randint(0, 25)] for x in [1]*size**2]
+print(mlist)
+'''
+for x in mlist:
+    x = alpha[x-1]
+print(mlist)
+'''
